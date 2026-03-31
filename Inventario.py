4043c84633b58c1e016ce798f7729e0c95d023cf
tomas <tomas.ep22@gmail.com>
2026-03-31 01:33:20 -0300

    Estructura basica funcion agregar item y funcion para buscar primer slot vacio

--- Inventario.py
+++ Inventario.py
@@ -13,3 +13,28 @@ def crear_inventario():
     for i in range(SLOTS_INVENTARIO):
         item = [0,"","",0,False] # [entero, string, string, entero, booleano]
         matriz_inventario.append(item)
+
+def buscar_primer_slot_vacio():
+    '''
+    Objetivo: Encontrar primer lugar vacio en la matriz
+    Salida: numero entero (el indice). devuelve -1 si no hay slots vacios
+    '''
+    for i in range(SLOTS_INVENTARIO):
+        if matriz_inventario[i][0] == 0:
+            return i
+    return -1
+
+def agregar_item(item):
+    '''
+    Entrada: lista
+    Objetivo: Agregar item al inventario
+    Salida: none
+    '''
+    if len(item) != COLUMNAS:
+        return
+    copia = item.copy()
+    indice = buscar_primer_slot_vacio()
+    if indice == -1:
+        return 'Inventario LLeno'
+    matriz_inventario[indice] = copia
+
