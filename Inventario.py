5fe7f8da8efb28a0afd94271c804304e1dd36847
tomas <tomas.ep22@gmail.com>
2026-04-09 04:07:21 -0300

    Funcion usar_item y manejar_equipado_item

--- Inventario.py
+++ Inventario.py
@@ -1,7 +1,7 @@
 INVENTARIO_MAXIMO = 30
 ITEM_KEYS = {'id_item':0,'nombre':'','tipo':'','cantidad':0,'equipado':False}
 
-# COLUMNAS = 5 # [id_item , nombre, tipo, cantidad, equipado] 
+# KEYS = 5 # [id_item , nombre, tipo, cantidad, equipado] 
 
 # tipos de items tentativos: consumible, equipable, intercambiable, clave, usable
 
@@ -29,12 +29,9 @@ def agregar_item(item,inventario):
             return print('Inventario lleno') # TODO: reemplazar por manejo de errores
         inventario.append(item.copy())
 
-    
-    
-
 def busqueda_item_por_id(id_item, inventario):
     '''
-    Entrada: entero (id del item a buscar)
+    Entrada: entero (id del item a buscar), lista (inventario)
     Objetivo: Buscar items secuencialmente segun ID
     Salida: Numero entero (indice del slot donde esta el item buscado o -1 si el item no esta presente)
     '''
@@ -46,3 +43,37 @@ def busqueda_item_por_id(id_item, inventario):
     else:
         return -1
 
+def usar_item(id_item, inventario):
+    '''
+    Entrada: entero (id del item a buscar), lista (inventario)
+    Objetivo: Manejar el uso de items
+    Salida: True o error
+    '''
+    i = busqueda_item_por_id(id_item, inventario)
+    if i == -1:
+        return print('item no se encuentra en el inventario')
+    if inventario[i]['tipo'] != 'consumible':
+        return print('El item no es consumible')
+    if inventario[i]['cantidad'] <= 0:
+        return print('No hay stock del item')
+    elif inventario[i]['cantidad'] == 1:
+        inventario.pop(i)
+    else:
+        inventario[i]['cantidad'] -= 1
+    return True
+    
+def manejar_equipado_item(id_item, inventario): # TODO agregar casos donde el subtipo de item ya esta equipado
+    '''
+    Entrada: entero (id del item a buscar), lista (inventario)
+    Objetivo: equipar/desquipar items
+    Salida: True o error
+    '''
+    i = busqueda_item_por_id(id_item, inventario)
+    if i == -1:
+        return print('item no se encuentra en el inventario')
+    if inventario[i]['tipo'] != 'equipable':
+        return print('El item no es equipable')
+    if inventario[i]['cantidad'] <= 0:
+        return print('No hay stock del item')
+    inventario[i]['equipado'] = not inventario[i]['equipado']
+    return True
