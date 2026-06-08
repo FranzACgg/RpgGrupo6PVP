fef84a618c90aeeb1a6274090acb311d6939f81e
tomas <tomas.ep22@gmail.com>
2026-06-08 02:02:35 -0300

    Agregar tests para busqueda_item_por_id y usar_item

--- test_inventario.py
+++ test_inventario.py
@@ -16,6 +16,8 @@ crear_inventario, agregar_item, busqueda_item_por_id, usar_item, manejar_equipad
 from inventario import (
     crear_inventario,
     agregar_item,
+    busqueda_item_por_id,
+    usar_item,
 )
 
 import pytest
@@ -113,3 +115,103 @@ def test_agregar_item():
         )
         is True
     )
+
+
+def test_busqueda_item_por_id():
+    assert (
+        busqueda_item_por_id(
+            1,
+            [
+                {
+                    "id_item": 1,
+                    "nombre": "Pocion de HP pequeña",
+                    "tipo": "consumible",
+                    "cantidad": 5,
+                    "equipado": False,
+                },
+            ],
+        )
+    ) == 0
+    assert (
+        busqueda_item_por_id(
+            1,
+            [
+                {
+                    "id_item": 2,
+                    "nombre": "Pocion de HP pequeña",
+                    "tipo": "consumible",
+                    "cantidad": 5,
+                    "equipado": False,
+                },
+            ],
+        )
+        == -1
+    )
+
+
+def test_usar_item():
+    assert usar_item(-1, []) is None
+    assert (
+        usar_item(
+            0,
+            [
+                {
+                    "id_item": 8,
+                    "nombre": "Lanza Maldita",
+                    "tipo": "equipable",
+                    "cantidad": 1,
+                    "equipado": False,
+                }
+            ],
+        )
+    ) is None
+    assert (
+        usar_item(
+            0,
+            [
+                {
+                    "id_item": 1,
+                    "nombre": "Pocion de HP pequeña",
+                    "tipo": "consumible",
+                    "cantidad": 0,
+                    "equipado": False,
+                }
+            ],
+        )
+    ) is None
+    assert usar_item(
+        1,
+        [
+            {
+                "id_item": 1,
+                "nombre": "Pocion de HP pequeña",
+                "tipo": "consumible",
+                "cantidad": 1,
+                "equipado": False,
+            }
+        ],
+    ) == {
+        "id_item": 1,
+        "nombre": "Pocion de HP pequeña",
+        "tipo": "consumible",
+        "cantidad": 1,
+        "equipado": False,
+    }
+    assert usar_item(
+        1,
+        [
+            {
+                "id_item": 1,
+                "nombre": "Pocion de HP pequeña",
+                "tipo": "consumible",
+                "cantidad": 5,
+                "equipado": False,
+            }
+        ],
+    ) == {
+        "id_item": 1,
+        "nombre": "Pocion de HP pequeña",
+        "tipo": "consumible",
+        "cantidad": 4,
+        "equipado": False,
+    }
